76ce1694103307c09a6318ea33a27af94dbea354
alexvolchkov <volkaa@mail.ru>
2021-11-24 16:42:13 +0300

    6.8. Дефрагментация массива [#225]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -16,6 +16,13 @@ public class SwitchArray {
         return array;
     }
 
+    public static String[] swap(String[] array, int source, int dest) {
+        String temp = array[dest];
+        array[dest] = array[source];
+        array[source] = temp;
+        return array;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
