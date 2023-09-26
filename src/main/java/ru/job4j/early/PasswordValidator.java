f367d3470d41105ef62ed3e6f3d65f0c8157f4c1
alexvolchkov <94671261+alexvolchkov@users.noreply.github.com>
2023-09-27 00:00:51 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,50 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+    private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
+
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password can't be null");
+        }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password should be length [8, 32]");
+        }
+        boolean hasUpCase = false;
+        boolean hasLowCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                hasLowCase = true;
+            }
+            if (Character.isUpperCase(c)) {
+                hasUpCase = true;
+            }
+            if (Character.isDigit(c)) {
+                hasDigit = true;
+            }
+            if (!Character.isLetterOrDigit(c)) {
+                hasSpecial = true;
+            }
+        }
+        if (!hasUpCase) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!hasLowCase) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
+        for (String s : FORBIDDEN) {
+            if (password.toLowerCase().contains(s)) {
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
+        }
+        return password;
+    }
+}
